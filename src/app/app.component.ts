75186a37175f5896d966b37b801c669658899d7c
Dylan <dylan.pre@usask.ca>
2017-01-21 17:33:38 -0600

    Initial sign-in page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,14 @@ import {Component} from '@angular/core';
 import {Platform} from 'ionic-angular';
 import {StatusBar, Splashscreen} from 'ionic-native';
 
-import {HomePage} from '../pages/home/home';
+import {SignInPage} from '../pages/sign-in/sign-in';
 
 
 @Component({
     templateUrl: 'app.html'
 })
 export class MyApp {
-    rootPage = HomePage;
+    rootPage = SignInPage;
 
     constructor(platform: Platform) {
         platform.ready().then(() => {
