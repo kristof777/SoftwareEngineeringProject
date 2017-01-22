0e2c3a07f7deeb0cb56f01d0abcbf789bf405ffd
KristofMercier <kmm732@mail.usask.ca>
2017-01-22 15:56:17 -0600

    Added declarations for the template pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,31 @@ import {MyApp} from './app.component';
 
 import {SignInPage} from '../pages/sign-in/sign-in'
 import {SignUpPage} from '../pages/sign-up/sign-up';
+import {EditListingsPage} from '../pages/edit-listings/edit-listings';
+import {FavouritesPage} from '../pages/favourites/favourites';
+import {FavouritesListPage} from '../pages/favourites-list/favourites-list';
+import {AddListingPage} from '../pages/add-listing/add-listing';
+import {FilterPage} from '../pages/filter/filter';
+import {MainPage} from '../pages/main/main';
+import {MyListingsPage} from '../pages/my-listings/my-listings';
+import {SettingsPage} from '../pages/settings/settings';
+
+
 
 @NgModule({
     declarations: [
         MyApp,
         SignInPage,
-        SignUpPage
+        SignUpPage,
+        AddListingPage,
+        EditListingsPage,
+        FavouritesPage,
+        FavouritesListPage,
+        FilterPage,
+        MainPage,
+        MyListingsPage,
+        SettingsPage
+
     ],
     imports: [
         IonicModule.forRoot(MyApp)
@@ -18,7 +37,16 @@ import {SignUpPage} from '../pages/sign-up/sign-up';
     entryComponents: [
         MyApp,
         SignInPage,
-        SignUpPage
+        SignUpPage,
+        AddListingPage,
+        EditListingsPage,
+        FavouritesPage,
+        FavouritesListPage,
+        FilterPage,
+        MainPage,
+        MyListingsPage,
+        SettingsPage
+
     ],
     providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
