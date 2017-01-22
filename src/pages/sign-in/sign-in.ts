905bccf0798ca00fe367f99479830520dd5834cb
Dylan <dylan.pre@usask.ca>
2017-01-21 20:10:11 -0600

    Linking buttons to methods.

--- src/pages/sign-in/sign-in.ts
+++ src/pages/sign-in/sign-in.ts
@@ -2,6 +2,8 @@ import {Component} from '@angular/core';
 
 import {NavController} from 'ionic-angular';
 
+import {SignUpPage} from '../sign-up/sign-up';
+
 @Component({
     selector: 'page-sign-in',
     templateUrl: 'sign-in.html'
@@ -10,5 +12,14 @@ export class SignInPage {
 
     constructor(public navCtrl: NavController) {
 
+    }
+
+    pushRegister(){
+        console.log("Register was clicked")
+        this.navCtrl.push(SignUpPage);
+    }
+
+    doSignIn(){
+        console.log("Sign In was clicked")
     }
 }
