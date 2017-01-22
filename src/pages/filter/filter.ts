fb0bdbdcf3bef62c7b155c93fa1cb9acb253a686
KristofMercier <kmm732@mail.usask.ca>
2017-01-22 15:33:18 -0600

    Added template pages for main and filter

---
+++ src/pages/filter/filter.ts
@@ -0,0 +1,16 @@
+let assert = require('assert-plus');
+import {Component} from '@angular/core';
+
+import {NavController} from 'ionic-angular';
+
+@Component({
+    selector: 'page-filter',
+    templateUrl: 'filter.html'
+})
+export class FilterPage {
+
+    constructor(public navCtrl: NavController) {
+
+    }
+
+}
---
+++ src/pages/main/main.ts
@@ -0,0 +1,16 @@
+let assert = require('assert-plus');
+import {Component} from '@angular/core';
+
+import {NavController} from 'ionic-angular';
+
+@Component({
+    selector: 'page-main',
+    templateUrl: 'main.html'
+})
+export class MainPage {
+
+    constructor(public navCtrl: NavController) {
+
+    }
+
+}
