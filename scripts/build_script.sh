d28ed05a2d751be33350901163883d4d350d9273
Chris Mykota-Reid <clm972@mail.usask.ca>
2017-01-22 22:30:38 -0600

    syntax
    my inability to spell android strikes again

--- scripts/build_script.sh
+++ scripts/build_script.sh
@@ -4,7 +4,7 @@ if [[ $TRAVIS_OS_NAME = 'osx' ]]; then
   ionic platform add ios
   ionic build
 else
-  ionic platform remove andriod
-  ionic platform add andriod
+  ionic platform remove android
+  ionic platform add android
   ionic build
 fi
