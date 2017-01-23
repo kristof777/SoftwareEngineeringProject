05f8a5254a14a0f954749c35367c6bdfa5aa8bd0
Chris Mykota-Reid <clm972@mail.usask.ca>
2017-01-22 19:34:14 -0600

    creating script for installation in travis [ci skip]
    conditionally installs packages depending on if it's ios or andriod

---
+++ scripts/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+    - brew update
+    - brew cask install java
+    - java -version
+    - echo $JAVA_HOME
+    - npm install -g grunt-cli cordova ionic
+    - npm install
+else
+  - npm install -g gulp bower cordova ionic
+  # installs packages specified in the ionic json
+  - npm install
+  - bower update
+  - sudo apt-get install oracle-java8-set-default
+fi
