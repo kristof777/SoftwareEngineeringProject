09153a5d4dfbbb4ff6bcf80041d8ddc890adffcb
Chris Mykota-Reid <clm972@mail.usask.ca>
2017-01-22 20:50:08 -0600

    fixing script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-    - brew update
-    - brew cask install java
-    - java -version
-    - echo $JAVA_HOME
-    - npm install -g grunt-cli cordova ionic
-    - npm install
+    brew update
+    brew cask install java
+    java -version
+    echo $JAVA_HOME
+    npm install -g grunt-cli cordova ionic
+    npm install
 else
-  - npm install -g gulp bower cordova ionic
+  npm install -g gulp bower cordova ionic
   # installs packages specified in the ionic json
-  - npm install
-  - bower update
-  - sudo apt-get install oracle-java8-set-default
+  npm install
+  bower update
+  sudo apt-get install oracle-java8-set-default
 fi
