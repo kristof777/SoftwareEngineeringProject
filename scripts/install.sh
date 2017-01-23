b247db88846d4ad55e8478df14597fc63d35a35e
Chris Mykota-Reid <clm972@mail.usask.ca>
2017-01-23 01:25:52 -0600

    uncommenting script [ci skip]
    if statements working as they should so now time to debug the package installation and build stuffs

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 if [ "$TRAVIS_OS_NAME" = "osx" ]; then
-    echo "got to install osx"
-    #brew update
-    #brew cask install java
-    #java -version
-    #echo $JAVA_HOME
-    #npm install -g grunt-cli cordova ionic
-    #npm install
+  echo "got to install osx"
+  brew update
+  brew cask install java
+  java -version
+  echo $JAVA_HOME
+  npm install -g grunt-cli cordova ionic
+  npm install
 else
   echo "got to install android"
-  #export JAVA_HOME=/usr/lib/jvm/java-8-oracle
-  #npm install -g gulp bower cordova ionic
+  export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+  npm install -g gulp bower cordova ionic
   # installs packages specified in the ionic json
-  #npm install
-  #bower update
-  #sudo apt-get install oracle-java8-set-default
+  npm install
+  bower update
+  sudo apt-get install oracle-java8-set-default
 fi
