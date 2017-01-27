6fd3f2bab0c148e323d112cdeb9015c62e6f2efe
Chris Mykota-Reid <clm972@mail.usask.ca>
2017-01-26 18:44:34 -0600

    commenting out ios and andriod builds [ci skip]
    making build times shorter for figuring out browser builds

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [ "$TRAVIS_OS_NAME" = "osx" ]; then
-  echo "got to install osx"
-  brew update
-  brew cask install java
-  java -version
-  echo $JAVA_HOME
-  npm install -g grunt-cli cordova ionic
-  npm install
+  #echo "got to install osx"
+  #brew update
+  #brew cask install java
+  #java -version
+  #echo $JAVA_HOME
+  #npm install -g grunt-cli cordova ionic
+  #npm install
 elif [ "$TRAVIS_OS_NAME" = "linux" ]; then
   echo "linux"
   export JAVA_HOME=/usr/lib/jvm/java-8-oracle
@@ -17,11 +17,11 @@ elif [ "$TRAVIS_OS_NAME" = "linux" ]; then
   bower update
   sudo apt-get install oracle-java8-set-default
 else
-  echo "got to install android"
-  export JAVA_HOME=/usr/lib/jvm/java-8-oracle
-  npm install -g gulp bower cordova ionic
+  #echo "got to install android"
+  #export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+  #npm install -g gulp bower cordova ionic
   # installs packages specified in the ionic json
-  npm install
-  bower update
-  sudo apt-get install oracle-java8-set-default
+  #npm install
+  #bower update
+  #sudo apt-get install oracle-java8-set-default
 fi
