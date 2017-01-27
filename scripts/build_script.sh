6f62a99643a5f21b90ab8d2c3b6616d3553d3854
Chris Mykota-Reid <clm972@mail.usask.ca>
2017-01-26 19:43:51 -0600

    added system info output to build script

--- scripts/build_script.sh
+++ scripts/build_script.sh
@@ -2,15 +2,31 @@
 
 if [ "$TRAVIS_OS_NAME" = "osx" ]; then
   echo "got to build OSX"
-  #ionic platform add ios
-  #ionic build
+  ionic platform add ios
+  ionic build
+  echo "************************System info************************"
+  xcodebuild -version
+  xcodebuild -showsdks
+  npm -v
+  npm ls
 elif [ "$TRAVIS_OS_NAME" = "linux" ]; then
   echo "the open source penguin"
   ionic platform add browser
   ionic build
+  echo "************************System info************************"
+  echo $TRAVIS_OS_NAME
+  java -version
+  npm -v
+  npm ls
 else
   echo "got to build android"
-  #ionic platform remove android
-  #ionic platform add android
-  #ionic build
+  ionic platform remove android
+  ionic platform add android
+  ionic build
+  echo "************************System info************************"
+  echo $TRAVIS_OS_NAME
+  java -version
+  andriod list target
+  npm -v
+  npm ls
 fi
