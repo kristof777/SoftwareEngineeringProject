5da05cbf145e7154c2e6165d6bca2956a2955fa3
Chris Mykota-Reid <clm972@mail.usask.ca>
2017-01-26 19:36:58 -0600

    uncommented other builds [ci skip]
    as far as I can tell, the browser build is working

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,12 +2,12 @@
 
 if [ "$TRAVIS_OS_NAME" = "osx" ]; then
   echo "got to install osx"
-  #brew update
-  #brew cask install java
-  #java -version
-  #echo $JAVA_HOME
-  #npm install -g grunt-cli cordova ionic
-  #npm install
+  brew update
+  brew cask install java
+  java -version
+  echo $JAVA_HOME
+  npm install -g grunt-cli cordova ionic
+  npm install
 elif [ "$TRAVIS_OS_NAME" = "linux" ]; then
   echo "got to linux install"
   export JAVA_HOME=/usr/lib/jvm/java-8-oracle
@@ -18,10 +18,10 @@ elif [ "$TRAVIS_OS_NAME" = "linux" ]; then
   sudo apt-get install oracle-java8-set-default
 else
   echo "got to install android"
-  #export JAVA_HOME=/usr/lib/jvm/java-8-oracle
-  #npm install -g gulp bower cordova ionic
-  # installs packages specified in the ionic json
-  #npm install
-  #bower update
-  #sudo apt-get install oracle-java8-set-default
+  export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+  npm install -g gulp bower cordova ionic
+  installs packages specified in the ionic json
+  npm install
+  bower update
+  sudo apt-get install oracle-java8-set-default
 fi
