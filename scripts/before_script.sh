746b73543270977fbce6f7b6231288d620f7526d
Chris Mykota-Reid <clm972@mail.usask.ca>
2017-01-26 19:38:01 -0600

    uncommenting other builds [ci skip]

--- scripts/before_script.sh
+++ scripts/before_script.sh
@@ -3,28 +3,28 @@
 if [ "$TRAVIS_OS_NAME" = "osx" ]; then
   # encrypting and decrypting keys goes here
   echo "should be osx"
-  #mkdir www
+  mkdir www
   #java -version
 elif [ "$TRAVIS_OS_NAME" = "linux" ]; then
   echo "got to linux before_script"
   mkdir www
-  java -version
+  #java -version
 else
   echo "should be andriod"
-  #wget http://dl.google.com/android/android-sdk_r24.3.4-linux.tgz
-  #echo $JAVA_HOME
-  #tar -xvf android-sdk_r24.3.4-linux.tgz
-  #echo y | ./android-sdk-linux/tools/android update sdk --no-ui --all --filter platform-tools
-  #echo y | ./android-sdk-linux/tools/android update sdk --no-ui --all --filter build-tools-23.0.3
-  #echo y | ./android-sdk-linux/tools/android update sdk --no-ui --all --filter android-24
-  #echo y | ./android-sdk-linux/tools/android update sdk --no-ui --all --filter extra-android-support
-  #echo y | ./android-sdk-linux/tools/android update sdk --no-ui --all --filter extra-android-m2repository
-  #echo y | ./android-sdk-linux/tools/android update sdk --no-ui --all --filter extra-google-m2repository
+  wget http://dl.google.com/android/android-sdk_r24.3.4-linux.tgz
+  echo $JAVA_HOME
+  tar -xvf android-sdk_r24.3.4-linux.tgz
+  echo y | ./android-sdk-linux/tools/android update sdk --no-ui --all --filter platform-tools
+  echo y | ./android-sdk-linux/tools/android update sdk --no-ui --all --filter build-tools-23.0.3
+  echo y | ./android-sdk-linux/tools/android update sdk --no-ui --all --filter android-24
+  echo y | ./android-sdk-linux/tools/android update sdk --no-ui --all --filter extra-android-support
+  echo y | ./android-sdk-linux/tools/android update sdk --no-ui --all --filter extra-android-m2repository
+  echo y | ./android-sdk-linux/tools/android update sdk --no-ui --all --filter extra-google-m2repository
   #ls $PWD
-  #export ANDROID_HOME=$PWD/android-sdk-linux
+  export ANDROID_HOME=$PWD/android-sdk-linux
   #echo $ANDROID_HOME
-  #mkdir www
+  mkdir www
   #java -version
   #ls /usr/lib/jvm/
-  #export PATH=${PATH}:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$ANDROID_HOME/build-tools/23.0.3
+  export PATH=${PATH}:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$ANDROID_HOME/build-tools/23.0.3
 fi
