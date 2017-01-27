129226062a2b06552a38d9a296fcc2627b36d33a
Chris Mykota-Reid <clm972@mail.usask.ca>
2017-01-26 18:42:37 -0600

    figuring out browser build [ci skip]

--- scripts/before_script.sh
+++ scripts/before_script.sh
@@ -2,27 +2,29 @@
 
 if [ "$TRAVIS_OS_NAME" = "osx" ]; then
   # encrypting and decrypting keys goes here
-  echo "should be osx"
+  #echo "should be osx"
+  #mkdir www
+  #java -version
+elif [ "$TRAVIS_OS_NAME" = "linux" ]; then
+  echo "got to linux before_script"
   mkdir www
   java -version
-elif [ "$TRAVIS_OS_NAME" = "linux" ]; then
-    echo "wah, i'm linux, i make the browser but no one has set me up yet! HMPH *crosses threads*"
 else
-  echo "should be andriod"
-  wget http://dl.google.com/android/android-sdk_r24.3.4-linux.tgz
-  echo $JAVA_HOME
-  tar -xvf android-sdk_r24.3.4-linux.tgz
-  echo y | ./android-sdk-linux/tools/android update sdk --no-ui --all --filter platform-tools
-  echo y | ./android-sdk-linux/tools/android update sdk --no-ui --all --filter build-tools-23.0.3
-  echo y | ./android-sdk-linux/tools/android update sdk --no-ui --all --filter android-24
-  echo y | ./android-sdk-linux/tools/android update sdk --no-ui --all --filter extra-android-support
-  echo y | ./android-sdk-linux/tools/android update sdk --no-ui --all --filter extra-android-m2repository
-  echo y | ./android-sdk-linux/tools/android update sdk --no-ui --all --filter extra-google-m2repository
+  #echo "should be andriod"
+  #wget http://dl.google.com/android/android-sdk_r24.3.4-linux.tgz
+  #echo $JAVA_HOME
+  #tar -xvf android-sdk_r24.3.4-linux.tgz
+  #echo y | ./android-sdk-linux/tools/android update sdk --no-ui --all --filter platform-tools
+  #echo y | ./android-sdk-linux/tools/android update sdk --no-ui --all --filter build-tools-23.0.3
+  #echo y | ./android-sdk-linux/tools/android update sdk --no-ui --all --filter android-24
+  #echo y | ./android-sdk-linux/tools/android update sdk --no-ui --all --filter extra-android-support
+  #echo y | ./android-sdk-linux/tools/android update sdk --no-ui --all --filter extra-android-m2repository
+  #echo y | ./android-sdk-linux/tools/android update sdk --no-ui --all --filter extra-google-m2repository
   #ls $PWD
   #export ANDROID_HOME=$PWD/android-sdk-linux
-  echo $ANDROID_HOME
-  mkdir www
-  java -version
-  ls /usr/lib/jvm/
-  export PATH=${PATH}:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$ANDROID_HOME/build-tools/23.0.3
+  #echo $ANDROID_HOME
+  #mkdir www
+  #java -version
+  #ls /usr/lib/jvm/
+  #export PATH=${PATH}:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$ANDROID_HOME/build-tools/23.0.3
 fi
