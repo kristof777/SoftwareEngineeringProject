86eb2e4969e7ce02d32e4bc0030bfcbf95bba98e
KristofMercier <kmm732@mail.usask.ca>
2017-01-28 16:25:57 -0600

    Added a button to register, as well as basic text verification.

--- src/pages/sign-up/sign-up.ts
+++ src/pages/sign-up/sign-up.ts
@@ -6,13 +6,15 @@ import {Logger} from "angular2-logger/core";
 
 import {NavController, ToastController} from 'ionic-angular';
 import {SignInPage} from "../sign-in/sign-in";
+import {MainPage} from "../main/main";
 
 @Component({
     selector: 'page-sign-up',
     templateUrl: 'sign-up.html'
 })
 export class SignUpPage {
-
+    email: string;
+    password: string;
     constructor(public navCtrl: NavController,
                 private _logger: Logger,
                 public toastCtrl: ToastController) {
@@ -22,6 +24,19 @@ export class SignUpPage {
     pushSignIn(){
         this._logger.debug("Sign In was clicked.");
         this.navCtrl.push(SignInPage);
+    }
+
+    doRegister(){
+        this._logger.debug("Sign In was clicked.")
+
+        if(this.email == "test")
+            this.navCtrl.setRoot(MainPage);
+        else
+            this.toastCtrl.create({
+                message: 'To continue, set e-mail to "test"',
+                duration: 3000,
+                position: 'top'
+            }).present();
     }
 
 }
