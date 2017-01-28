a9fcf18db15b18793be72b6f6f31c070b519f698
Chris Mykota-Reid <clm972@mail.usask.ca>
2017-01-28 14:22:07 -0600

    still working on android
    checked available packages when using android update and it looks like the most recent packages aren't available so it'd make sense why these things weren't getting downloaded when i specified them

--- scripts/before_script.sh
+++ scripts/before_script.sh
@@ -15,11 +15,11 @@ else
   echo $JAVA_HOME
   tar -xf android-sdk_r24.4-linux.tgz
   ./android-sdk-linux/tools/android update sdk --no-ui --all --filter platform-tools-23.0.1 
-  ./android-sdk-linux/tools/android update sdk --no-ui --all --filter build-tools-24.0.3 >android_ouput.txt
-  ./android-sdk-linux/tools/android update sdk --no-ui --all --filter android-24 >android_ouput.txt
-  ./android-sdk-linux/tools/android update sdk --no-ui --all --filter extra-android-support >android_ouput.txt
-  ./android-sdk-linux/tools/android update sdk --no-ui --all --filter extra-android-m2repository >android_ouput.txt
-  ./android-sdk-linux/tools/android update sdk --no-ui --all --filter extra-google-m2repository >android_ouput.txt
+  ./android-sdk-linux/tools/android update sdk --no-ui --all --filter build-tools-23.0.1
+  ./android-sdk-linux/tools/android update sdk --no-ui --all --filter android-23 
+  ./android-sdk-linux/tools/android update sdk --no-ui --all --filter extra-android-support 
+  ./android-sdk-linux/tools/android update sdk --no-ui --all --filter extra-android-m2repository 
+  ./android-sdk-linux/tools/android update sdk --no-ui --all --filter extra-google-m2repository
   ls 
   echo $ANDROID_HOME
   mkdir www
