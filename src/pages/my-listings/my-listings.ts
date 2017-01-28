82c6647877d104113a861b3523ae948a159651d4
Gaurav Arora (gaa721) <gaurav.arora@usask.ca>
2017-01-28 15:30:34 -0600

    Added my-listing page, and saved-listing-provider

--- src/pages/my-listings/my-listings.ts
+++ src/pages/my-listings/my-listings.ts
@@ -1,16 +1,29 @@
 let assert = require('assert-plus');
 import {Component} from '@angular/core';
-
 import {NavController} from 'ionic-angular';
+import {Location} from "./location";
+import {SavedListingProvider} from "../../app/providers/saved-listings-provider";
+import {Listing} from '../../app/models/listing';
+import {Logger} from "angular2-logger/core";
+
+
 
 @Component({
     selector: 'page-my-listings',
-    templateUrl: 'my-listings.html'
+    templateUrl: 'my-listings.html',
+    providers: [SavedListingProvider]
 })
 export class MyListingsPage {
 
-    constructor(public navCtrl: NavController) {
+    storedData: Listing[];
 
+    constructor(public navCtrl: NavController, public sListings: SavedListingProvider, private _logger: Logger) {
+        this.storedData = sListings.data;
+    }
+
+    haveFun(listingId:number){
+        // open about that listing
+        this._logger.debug("Listing Id " + listingId +" was clicked");
     }
 
 }
