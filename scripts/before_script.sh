4e8f66ea7dce5378b79b9911ed9e8ad7bcd6b387
Chris Mykota-Reid <clm972@mail.usask.ca>
2017-01-27 21:12:43 -0600

    looks like google is removing support from wget
    can't get android version 25 or higher using wget.. might have to figure out a work around with travis, for now going to try and get the build going with android 24

--- scripts/before_script.sh
+++ scripts/before_script.sh
@@ -11,20 +11,19 @@ elif [ "$TRAVIS_OS_NAME" = "linux" ]; then
   #java -version
 else
   echo "should be andriod"
-  wget http://dl.google.com/android/repository/tools_r25.2.3-linux.zip
+  wget http://dl.google.com/android/android-sdk_r24.4-linux.tgz
   echo $JAVA_HOME
-  unzip -qq tools_r25.2.3-linux.zip
-  echo y | ./tools/android update sdk --no-ui --all --filter platform-tools
-  echo y | ./tools/android update sdk --no-ui --all --filter build-tools-24.0.3
-  echo y | ./tools/android update sdk --no-ui --all --filter android-25
-  echo y | ./tools/android update sdk --no-ui --all --filter extra-android-support
-  echo y | ./tools/android update sdk --no-ui --all --filter extra-android-m2repository
-  echo y | ./tools/android update sdk --no-ui --all --filter extra-google-m2repository
-  cd tools
+  tar -xvf android-sdk_r24.4-linux.tgz
+  echo y | ./android-sdk-linux/tools/android update sdk --no-ui --all --filter platform-tools
+  echo y | ./android-sdk-linux/tools/android update sdk --no-ui --all --filter build-tools-24.0.3
+  echo y | ./android-sdk-linux/tools/android update sdk --no-ui --all --filter android-24
+  echo y | ./android-sdk-linux/tools/android update sdk --no-ui --all --filter extra-android-support
+  echo y | ./android-sdk-linux/tools/android update sdk --no-ui --all --filter extra-android-m2repository
+  echo y | ./android-sdk-linux/tools/android update sdk --no-ui --all --filter extra-google-m2repository
   ls 
   echo $ANDROID_HOME
   mkdir www
   #java -version
   #ls /usr/lib/jvm/
-  export PATH=${PATH}:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$ANDROID_HOME/build-tools/23.0.3
+  export PATH=${PATH}:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$ANDROID_HOME/build-tools/24.0.3
 fi
