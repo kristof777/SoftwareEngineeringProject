d3b6973797e216ad205cbff81230674af60f1a42
Dylan <dylan.pre@usask.ca>
2017-01-31 21:53:36 -0600

    [Skip CI] Adding a province enum.

--- src/app/models/location.ts
+++ src/app/models/location.ts
@@ -1,12 +1,14 @@
+import {Province} from "./province";
+
 export class Location{
-    public province: string;
+    public province: Province;
     public city: string;
     public address: string;
     public postalCode: string;
     public long: number;
     public lat: number;
 
-    constructor(province: string, city: string, address: string, postalCode: string, long: number,
+    constructor(province: Province, city: string, address: string, postalCode: string, long: number,
                 lat: number){
         this.province = province;
         this.city = city;
---
+++ src/app/models/province.ts
@@ -0,0 +1,16 @@
+export class Province{
+
+    static AB = { abbr: "AB", name: "Alberta"};
+    static BC = { abbr: "BC", name: "British Columbia"};
+    static MB = { abbr: "MB", name: "Manitoba"};
+    static NB = { abbr: "NB", name: "New Brunswick"};
+    static NL = { abbr: "NL", name: "Newfoundland and Labrador"};
+    static NS = { abbr: "NS", name: "Nova Scotia"};
+    static NU = { abbr: "NU", name: "Nunavut"};
+    static NW = { abbr: "NW", name: "North West Territories"};
+    static ON = { abbr: "ON", name: "Ontario"};
+    static PE = { abbr: "PE", name: "Prince Edward Island"};
+    static QC = { abbr: "QC", name: "Quebec"};
+    static SK = { abbr: "SK", name: "Saskatchewan"};
+    static YT = { abbr: "YT", name: "Yukon"};
+}
