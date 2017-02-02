f39f4d872752e58fe76c754a12db48c1b4364996
Gaurav Arora (gaa721) <gaurav.arora@usask.ca>
2017-02-02 01:18:58 -0600

    Start of segment bar, need to make it better somehow

--- src/pages/my-listings/my-listings.ts
+++ src/pages/my-listings/my-listings.ts
@@ -2,7 +2,8 @@ let assert = require('assert-plus');
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
 import {Location} from "./location";
-import {SavedListingProvider} from "../../app/providers/saved-listings-provider";
+import {SavedFavListingProvider} from "../../app/providers/saved-fav-listings-provider";
+import {MineListingProvider} from "../../app/providers/saved-mine-listings-provider";
 import {Listing} from '../../app/models/listing';
 import {Logger} from "angular2-logger/core";
 
@@ -11,19 +12,30 @@ import {Logger} from "angular2-logger/core";
 @Component({
     selector: 'page-my-listings',
     templateUrl: 'my-listings.html',
-    providers: [SavedListingProvider]
+    providers: [SavedFavListingProvider,MineListingProvider]
+
 })
 export class MyListingsPage {
 
     storedData: Listing[];
 
-    constructor(public navCtrl: NavController, public sListings: SavedListingProvider, private _logger: Logger) {
+    constructor(public navCtrl: NavController, public sListings: SavedFavListingProvider,public mListings: MineListingProvider ,private _logger: Logger) {
         this.storedData = sListings.data;
     }
 
     haveFun(listingId:number){
         // open about that listing
         this._logger.debug("Listing Id " + listingId +" was clicked");
+    }
+
+    selectListings(){
+        this._logger.debug("Listing menu selected");
+        this.storedData = this.sListings.data;
+    }
+
+    selectFavourites(){
+        this._logger.debug("Favourites menu selected");
+        this.storedData = this.mListings.data;
     }
 
 }
