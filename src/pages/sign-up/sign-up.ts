8549e22102d4a10d9def197a19b2b401f71fd76d
Dylan <dylan.pre@usask.ca>
2017-02-02 12:42:05 -0600

    [Skip CI] Coding style

--- src/pages/sign-up/sign-up.ts
+++ src/pages/sign-up/sign-up.ts
@@ -27,86 +27,78 @@ export class SignUpPage {
     }
 
     doRegister() {
-        this._logger.debug("Register was clicked.")
-        var message = null
-        var passwordStrength = null
-        if (this.email == null || !(this.checkEmail(this.email))) {
-            message = "Please enter a valid E-mail address"
-        } else if (!this.confirmTwo(this.email, this.confirmEmail)) {
-            message = "E-mails do not match"
-        } else
-            if (this.password == null){
-            message = "Please enter a password"
-        }
-        else {
-            passwordStrength = this.checkPass(this.password)
-            if (passwordStrength != null && passwordStrength < 4) {
+        this._logger.debug("Register was clicked.");
+        var message = null;
+        var passwordStrength = null;
 
+        if (!this.email || !(this.checkEmail(this.email))) {
+            message = "Please enter a valid E-mail address";
+        } else if (!this.confirmTwo(this.email, this.confirmEmail)) {
+            message = "E-mails do not match";
+        } else if (!this.password){
+            message = "Please enter a password";
+        } else {
+            passwordStrength = this.checkPass(this.password);
+
+            if (passwordStrength != null && passwordStrength < 4) {
                 if (passwordStrength == 0) {
-                    message = "must include at least one lower case letter"
-                }
-                else if (passwordStrength == 1) {
-                    message = "must include at least one upper case letter"
-                }
-                else if (passwordStrength == 2) {
-                    message = "must include at least one number"
-                }
-                else if (passwordStrength == 3) {
-                    message = "must be longer than 7 characters"
+                    message = "Password must include at least one lower case letter";
+                } else if (passwordStrength == 1) {
+                    message = "Password must include at least one upper case letter";
+                } else if (passwordStrength == 2) {
+                    message = "Password must include at least one number";
+                } else if (passwordStrength == 3) {
+                    message = "Password must be longer than 7 characters";
                 }
             } else if (!this.confirmTwo(this.password, this.confirmPassword)) {
-                message = "Passwords do not match"
-            } else if (this.firstName == null){
-                message = "Please enter your first name"
-
-            }else if (this.lastName == null){
-                message = "Please enter your last name"
-
-            }else if (this.phoneNumber == null){
-                message = "Please enter your phone-number"
-
-            }else if (this.city == null){
-                message = "Please enter the name of your city"
-
+                message = "Passwords do not match";
+            } else if (!this.firstName){
+                message = "Please enter your first name";
+            } else if (!this.lastName){
+                message = "Please enter your last name";
+            } else if (!this.phoneNumber){
+                message = "Please enter your phone-number";
+            } else if (!this.city){
+                message = "Please enter the name of your city";
             }
-
         }
-        if (message != null) {
+
+        if (message) {
             this.toastCtrl.create({
                 message: message,
                 duration: 3000,
                 position: 'top'
             }).present();
-        }
-        else{
+        } else {
             //TODO Sign in stuff
             this.navCtrl.setRoot(MainPage);
         }
     }
-
-
 
     //password must contain at leas one lower case, upper case, and numeric character,
     // and must be at least 7 characters long
     //:Precond string must be a valid, non null string.
     //:Returns 0 if success, 1 if no lower case letter, 2 if no upper case, 3 if no numeric, 4 if the password is below 7 characters.
     checkPass(password: string){
-        assert (password != null)
-        var lowerCase = new RegExp("^(?=.*[a-z])")
-        var upperCase = new RegExp("^(?=.*[A-Z])")
-        var numeric = new RegExp("^(?=.*[0-9])")
-        var length = new RegExp("^(?=.{7,})")
-        if(!lowerCase.test(password)){
-            return 0
-        }if(!upperCase.test(password)){
-            return 1
-        }if(!numeric.test(password)){
-            return 2
-        }if(!length.test(password)){
-            return 3
-        }
-        return 4
+        assert (password != null);
+        var lowerCase = new RegExp("^(?=.*[a-z])");
+        var upperCase = new RegExp("^(?=.*[A-Z])");
+        var numeric = new RegExp("^(?=.*[0-9])");
+        var length = new RegExp("^(?=.{7,})");
 
+        if(!lowerCase.test(password)){
+            return 0;
+        }
+        if(!upperCase.test(password)){
+            return 1;
+        }
+        if(!numeric.test(password)){
+            return 2;
+        }
+        if(!length.test(password)){
+            return 3;
+        }
+        return 4;
     }
 
     confirmTwo(firstField: string, secondField: string){
@@ -114,7 +106,7 @@ export class SignUpPage {
     }
 
     checkEmail(email: string){
-        if (email == null){
+        if (!email){
             return false
         }
         var regExp = new RegExp("^(.+)@(.+){2,}\.(.+){2,}")
