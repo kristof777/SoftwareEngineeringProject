e8ac0840bd47020029da75758d2ff77e383e2fb0
KristofMercier <kmm732@mail.usask.ca>
2017-02-01 21:50:50 -0600

    Added an extensive email validation regex and custom error messages for input

--- src/pages/sign-up/sign-up.ts
+++ src/pages/sign-up/sign-up.ts
@@ -26,32 +26,46 @@ export class SignUpPage {
 
     }
 
-    doRegister(){
+    doRegister() {
         this._logger.debug("Sign Up was clicked.")
-
+        var message
         var passwordStrength = this.checkPass(this.password)
-        if(!this.checkEmail(this.email)){
-            this.toastCtrl.create({
-                message: 'Invalid Email address',
-                duration: 3000,
-                position: 'top'
-            }).present();
-        }else if(passwordStrength < 4){
-            //add messages for each case
+        assert(passwordStrength >= 0 && passwordStrength <= 4)
+        if (!this.checkEmail(this.email)) {
+            message = "Please enter a valid E-mail address"
+        } else if (passwordStrength < 4) {
 
+            if (passwordStrength == 0) {
+                message = "must include at least one lower case letter"
+            }
+            else if (passwordStrength == 1) {
+                message = "must include at least one upper case letter"
+            }
+            else if (passwordStrength == 2) {
+                message = "must include at least one number"
+            }
+            else if (passwordStrength == 3) {
+                message = "must be longer than 7 characters"
+            }
+        } else if (!this.confirmTwo(this.email, this.confirmEmail)) {
+            message = "E-mails do not match"
+        } else if (!this.confirmTwo(this.password, this.confirmPassword)) {
+            message = "Passwords do not match"
         }
-        else if(!this.confirmTwo(this.email, this.confirmEmail)){
-            this.toastCtrl.create({
-                message: 'To continue, set e-mail to "test"',
-                duration: 3000,
-                position: 'top'
-            }).present();
-        }
+
         else {
             this.navCtrl.setRoot(MainPage);
         }
-
+        if (message != null) {
+            this.toastCtrl.create({
+                message: message,
+                duration: 3000,
+                position: 'top'
+            }).present();
+        }
     }
+
+
 
     //password must contain at leas one lower case, upper case, and numeric character,
     // and must be at least 7 characters long
@@ -82,7 +96,11 @@ export class SignUpPage {
 
     checkEmail(email: string){
         assert (email != null)
-        var regExp = new RegExp("^[a-zA-Z0–9_.+-]+@[a-zA-Z0–9-]+.[a-zA-Z0–9-.]+$")
+        var regExp = new RegExp("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`" +
+            "{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])" +
+            "*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4]" +
+            "[0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:" +
+            "[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])")
         return regExp.test(email)
     }
 
