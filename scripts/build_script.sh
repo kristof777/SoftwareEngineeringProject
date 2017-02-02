861e4f0836de50a6f739c49bbfacc2f6ecf6cc75
Chris Mykota-Reid <clm972@mail.usask.ca>
2017-02-02 16:37:16 -0600

    added android versions output to build [ci skip]

--- scripts/build_script.sh
+++ scripts/build_script.sh
@@ -29,6 +29,12 @@ else
   ionic emulate android
   echo "************************System info************************"
   echo $TRAVIS_OS_NAME
+  echo "SDK Platform Android 7.1.1, API 25, revision 3"
+  echo "Android SDK Tools, revision 25.2.5"
+  echo "Android SDK Build-tools, revision 25.0.1"
+  echo "Android SDK Platform-tools, revision 25.0.3"
+  echo "Google Repository, revision 42"
+  echo "Android Support Repository, revision 42"
   java -version
   andriod list target
   echo "npm version:"
