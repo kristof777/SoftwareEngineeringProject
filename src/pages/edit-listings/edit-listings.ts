86f7f3c6864d16953555d7a2224368956adec542
Gaurav Arora (gaa721) <gaurav.arora@usask.ca>
2017-02-04 00:21:15 -0600

    listing can be deleted and edited using swipe

--- src/pages/edit-listings/edit-listings.ts
+++ src/pages/edit-listings/edit-listings.ts
@@ -1,9 +1,7 @@
 import {Logger} from "angular2-logger/core";
 let assert = require('assert-plus');
 import {Component} from '@angular/core';
-
-import {NavController} from 'ionic-angular';
-
+import {NavController, NavParams} from 'ionic-angular';
 @Component({
     selector: 'page-edit-listings',
     templateUrl: 'edit-listings.html'
@@ -20,9 +18,19 @@ export class EditListingsPage {
     description: string;
 
     constructor(public navCtrl: NavController,
-                private _logger: Logger) {
+                private _logger: Logger,
+                public navparams: NavParams) {
+        let listing = navparams.get("data");
+        this.bathrooms = listing['bathrooms'];
+        this.province = listing['location']['province'];
+        this.bedrooms = listing['bedrooms'];
+        this.squarefeet = listing['squarefeet'];
+        this.price = listing['price'];
+        this.address = listing['location']['address'];
+        this.description = listing['description'];
+        this.city = listing['location']['city'];
 
-        }
+    }
 
         save(){
 
