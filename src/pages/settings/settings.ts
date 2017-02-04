bc56282941030639f4f64d0cd836d4a543ee8719
Dylan <dylan.pre@usask.ca>
2017-02-03 18:42:14 -0600

    [Skip CI] Added province field in settings

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -2,6 +2,8 @@ import {User} from "../../app/models/user";
 let assert = require('assert-plus');
 import {Component} from '@angular/core';
 import {Logger} from "angular2-logger/core";
+import {Location} from "../../app/models/location";
+import {Province} from "../../app/models/province";
 
 import {NavController} from 'ionic-angular';
 
@@ -10,11 +12,21 @@ import {NavController} from 'ionic-angular';
     templateUrl: 'settings.html'
 })
 export class SettingsPage {
-    currentUser: User
+    private provinces: Province[];
+    currentUser: User;
+
     constructor(public navCtrl: NavController,
                 private _logger: Logger) {
+        this.provinces = Province.asArray;
         //TODO: Remove fake user account
-        this.currentUser = new User(null, 'test@gmail.com', 'Test', 'Test', '3062325323', null, null, null);
+        let userID: number = 1;
+        let email: string = "john.doe@gmail.com";
+        let firstName: string = "John";
+        let lastName: string = "Doe";
+        let phoneNumber: string = "3065555555";
+        let location: Location = new Location(Province.SK, "Saskatoon", "1234 Saskatoon St.", "A1B2C3", 0.0, 0.0);
+
+        this.currentUser = new User(userID, email, firstName, lastName, phoneNumber, null, null, location);
     }
 
     saveChanges() {
