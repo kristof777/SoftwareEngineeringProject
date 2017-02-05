c8db76f15749b1bd4bee8ded30ead635cdb642b7
Chris Mykota-Reid <dgtl.chris@gmail.com>
2017-02-05 13:21:26 -0600

    i think this decripts the key but I'm not 100% sure, travis just told me to add it to the yml

--- scripts/before_deploy.sh
+++ scripts/before_deploy.sh
@@ -4,6 +4,9 @@
 export PUSH_FOLDER=/home/travis/build/CMPT371Team1/Project/releases
 export BUILD_FOLDER=/home/travis/build/CMPT371Team1/Project/platforms/android/build/outputs
 
+openssl aes-256-cbc -K $encrypted_fed185e319aa_key -iv
+$encrypted_fed185e319aa_iv -in github_deploy_key.enc -out github_deploy_key -d
+
 # Create folder to deploy to
 cd $PUSH_FOLDER
 mkdir $TRAVIS_BUILD_ID
