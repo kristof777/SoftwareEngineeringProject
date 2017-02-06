a7b9e7bb44a34ebc639a067f97d281bb4ae6aad9
Chris Mykota-Reid <clm972@mail.usask.ca>
2017-02-05 23:48:33 -0600

    manually merging master in develop [ci skip]

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
-if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-    brew update
-    brew cask install java
-    java -version
-    echo $JAVA_HOME
-    npm install -g grunt-cli cordova ionic
-    npm install
-else
-  export JAVA_HOME=/usr/lib/jvm/java-8-oracle
-  npm install -g gulp bower cordova ionic
+if [ "$TRAVIS_OS_NAME" = "osx" ]; then
+  echo "got to install osx"
+  brew update
+  brew cask install java
+  java -version
+  echo $JAVA_HOME
+  npm install -g grunt-cli cordova ionic
+  npm install
+elif [ "$TRAVIS_OS_NAME" = "linux" ]; then
+  echo "got to linux install"
+  sudo apt-get install oracle-java8-set-default
+  npm install -g bower cordova ionic
   # installs packages specified in the ionic json
   npm install
   bower update
+else
+  echo "got to install android"
   sudo apt-get install oracle-java8-set-default
+  npm install -g bower cordova ionic
+  # installs packages specified in the ionic json
+  npm install
+  bower update
 fi
