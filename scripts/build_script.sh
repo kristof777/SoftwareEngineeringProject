d2aeda22f253ecce16fa5b898dfd5362b219cc32
Chris Mykota-Reid <clm972@mail.usask.ca>
2017-02-05 21:19:25 -0600

    moved --release so different spot
    was getting an error that you can't build ios when not on a mac osx but that shouldn't be getting built so I think it's an error somewhere else causing this

--- scripts/build_script.sh
+++ scripts/build_script.sh
@@ -36,7 +36,7 @@ else
   ionic platform remove android
   ionic platform add android
   if [ "$BUILD_TYPE" = "deployment" ]; then
-    ionic build --release android
+    ionic build android --release
     ls /home/travis/build/CMPT371Team1/Project/platforms/android/build/outputs/
   else
     ionic build android
