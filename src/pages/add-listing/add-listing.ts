ba12d128c25cce202552f7a66f9a9f4718723ddf
Dylan P <dylan.pre@usask.ca>
2017-02-10 13:03:58 -0600

    Adding the image selector to the add-listing page.

--- src/pages/add-listing/add-listing.ts
+++ src/pages/add-listing/add-listing.ts
@@ -1,8 +1,8 @@
 import {Logger} from "angular2-logger/core";
 let assert = require('assert-plus');
 import {Component} from '@angular/core';
-
 import {NavController} from 'ionic-angular';
+import {Camera} from 'ionic-native';
 
 @Component({
     selector: 'page-add-listing',
@@ -17,6 +17,7 @@ export class AddListingPage {
     price: number;
     address: string;
     description: string;
+    images: string[];
 
     constructor(public navCtrl: NavController,
                 private _logger: Logger) {
@@ -27,8 +28,28 @@ export class AddListingPage {
 
     }
 
-    uploadImage(){
+    /**
+     * Display the UI to add the image
+     */
+    addImage(){
+        const options = {
+            // Turn image to byte64
+            destinationType: Camera.DestinationType.DATA_URL,
+            // Pick image from library
+            sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+            // Dimensions to scale image to
+            targetWidth: 1280,
+            targetHeight: 720
+        };
 
+        Camera.getPicture(options).then((data) => {
+            // This should be the byte64 representation of the image.
+            console.log(data);
+            // Add the data to the end of the images array.
+            this.images[this.images.length] = data;
+        }, (error) => {
+            console.log(error);
+        });
     }
 
     publish(){
