b2ed4680fa5780a11b7674779509dbcadb439000
Dylan <dylan.pre@usask.ca>
2017-02-10 18:06:31 -0600

    Data Structure Changes - Remove phone number 3 from user

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -7,7 +7,6 @@ export class User{
     public lastName: string;
     public phone1: string;
     public phone2: string;
-    public phone3: string;
     public location: Location;
 
     /**
@@ -18,17 +17,15 @@ export class User{
      * @param lastName
      * @param phone1
      * @param phone2
-     * @param phone3
      * @param location
      */
     constructor(id: number, email: string, firstName: string, lastName: string, phone1: string,
-                phone2: string, phone3: string, location: Location) {
+                phone2: string, location: Location) {
         this.email = email;
         this.firstName = firstName;
         this.lastName = lastName;
         this.phone1 = phone1;
         this.phone2 = phone2;
-        this.phone3 = phone3;
         this.location = location;
     }
 }
