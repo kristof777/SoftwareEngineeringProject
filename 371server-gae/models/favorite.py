2306fb0e59a9094ff739d0dee900806d20c2e5ec
Melody Zhao <tiz466@mail.usask.ca>
2017-02-17 22:09:12 -0600

    removed favorite.py temporary

--- 371server-gae/models/favorite.py
+++
@@ -1,8 +0,0 @@
-from google.appengine.ext import ndb
-
-
-class Favorite(ndb.Model):
-    """Models an individual Guestbook entry with content and date."""
-    listingId = ndb.IntegerProperty(required=True)
-    userId = ndb.IntegerProperty(required=True)
-    liked = ndb.BooleanProperty(required=True, default=True)
