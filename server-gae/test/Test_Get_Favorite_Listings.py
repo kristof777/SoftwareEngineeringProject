e2fc135263e4da5467ffbc738621826fd8f24808
Melody Zhao <tiz466@mail.usask.ca>
2017-02-28 15:22:00 -0600

    refactored unit test cases in get favourites

--- server-gae/test/Test_Get_Favorite_Listings.py
+++ server-gae/test/Test_Get_Favorite_Listings.py
@@ -1,13 +1,11 @@
 from __future__ import absolute_import
 import unittest
-import json
 import os
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 import extras.Error_Code as Error_Code
 import Main
 from google.appengine.ext import testbed
 from web_apis.Create_User import *
-import extras.utils as utils
 
 
 class TestHandlers(unittest.TestCase):
@@ -21,71 +19,41 @@ class TestHandlers(unittest.TestCase):
         self.testbed.init_datastore_v3_stub()
         self.testbed.init_memcache_stub()
 
-        # create 10 listings for a user
+        # create 10 listings for one user
         listings, users = create_dummy_listings_for_testing(Main, 10)
         assert len(users) == 1
         assert len(listings) == 10
 
-        # now create a new user as a liker
+        # now create a new user as a liker, we'll use this person to send getFavorites request
         users = create_dummy_users_for_testing(1, Main)
         assert len(users) == 1
         liker = users[0]
         self.likerId = liker['userId']
 
         # make the liker likes the first five of the listings
-        likeTheListing = {
-            "userId": self.likerId,
-            "listingId": listings[0]['listingId'],
-            "liked": "True"
-        }
 
-        request = webapp2.Request.blank('/like', POST=likeTheListing)
-        response = request.get_response(Main.app)
-        self.assertEquals(response.status_int, 200)
+        res_value, status = get_like_response(get_like_post_dictionary(self.likerId, listings[0]['listingId'], "True"))
+        self.assertEqual(status, success)
+        self.assertEquals(res_value, None)
 
-        likeTheListing = {
-            "userId": self.likerId,
-            "listingId": listings[1]['listingId'],
-            "liked": "True"
-        }
+        res_value, status = get_like_response(get_like_post_dictionary(self.likerId, listings[1]['listingId'], "True"))
+        self.assertEqual(status, success)
+        self.assertEquals(res_value, None)
 
-        request = webapp2.Request.blank('/like', POST=likeTheListing)
-        response = request.get_response(Main.app)
-        self.assertEquals(response.status_int, 200)
+        res_value, status = get_like_response(get_like_post_dictionary(self.likerId, listings[2]['listingId'], "True"))
+        self.assertEqual(status, success)
+        self.assertEquals(res_value, None)
 
-        likeTheListing = {
-            "userId": self.likerId,
-            "listingId": listings[2]['listingId'],
-            "liked": "True"
-        }
+        res_value, status = get_like_response(get_like_post_dictionary(self.likerId, listings[3]['listingId'], "True"))
+        self.assertEqual(status, success)
+        self.assertEquals(res_value, None)
 
-        request = webapp2.Request.blank('/like', POST=likeTheListing)
-        response = request.get_response(Main.app)
-        self.assertEquals(response.status_int, 200)
+        res_value, status = get_like_response(get_like_post_dictionary(self.likerId, listings[4]['listingId'], "True"))
+        self.assertEqual(status, success)
+        self.assertEquals(res_value, None)
 
-        likeTheListing = {
-            "userId": self.likerId,
-            "listingId": listings[3]['listingId'],
-            "liked": "True"
-        }
-
-        request = webapp2.Request.blank('/like', POST=likeTheListing)
-        response = request.get_response(Main.app)
-        self.assertEquals(response.status_int, 200)
-
-        likeTheListing = {
-            "userId": self.likerId,
-            "listingId": listings[4]['listingId'],
-            "liked": "True"
-        }
-
-        request = webapp2.Request.blank('/like', POST=likeTheListing)
-        response = request.get_response(Main.app)
-        self.assertEquals(response.status_int, 200)
-
-        # the first five listings has been liked
+        # the first five listings has been liked, so we create a list of those listings for future use
         self.likedListings = listings[0:5]
-
 
     def test_get_fav_listings(self):
 
@@ -101,6 +69,7 @@ class TestHandlers(unittest.TestCase):
         self.assertEquals(response.status_int, 200)
         output = json.loads(response.body)
 
+        # get the number of published listings
         isPublishes = 0
         for listing in self.likedListings:
             if listing['isPublished'] == 'True':
@@ -137,6 +106,21 @@ class TestHandlers(unittest.TestCase):
         # completed. If the testbed is not deactivated, the original
         # stubs will not be restored.
         self.testbed.deactivate()
+
+
+def get_like_post_dictionary(userId, listingId, liked):
+    return {"userId": userId, "listingId":
+        listingId, "liked": liked}
+
+
+def get_like_response(POST):
+    request = webapp2.Request.blank('/like', POST=POST)
+    response = request.get_response(Main.app)
+    if response.body:
+        return json.loads(response.body), response.status_int
+    else:
+        return None, response.status_int
+
 
 def run_tests():
     unittest.main()
