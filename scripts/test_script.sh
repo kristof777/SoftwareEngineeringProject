8ea5e0af4dcd14800a7f82e9497fc8e2c1db0aba
Chris Mykota-Reid <clm972@mail.usask.ca>
2017-03-02 15:15:20 -0600

    adding descriptor echo's to test_script [ci skip]

--- scripts/test_script.sh
+++ scripts/test_script.sh
@@ -4,7 +4,10 @@
 # A helper script to allow both smoke tests to run even if one fails
 # while still having the build fail if either fails
 
+echo "*******RUNNING BACK-END SMOKE TESTS*******"
 python -m unittest discover -s server-gae/ -p 'Test*.py'
+
+echo "*******RUNNING FRONT-END SMOKE TESTS*******"
 screen -d -m -L ionic serve --firefox@47.0.1
 sleep 50
 xvfb-run protractor e2e-tests.conf.js
