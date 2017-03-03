90b7c262e37f8bfcf9b722cc592907fbab4bd251
Chris Mykota-Reid <clm972@mail.usask.ca>
2017-03-03 00:31:57 -0600

    added newline characters to echo

--- scripts/test_script.sh
+++ scripts/test_script.sh
@@ -4,7 +4,7 @@
 # A helper script to allow both smoke tests to run even if one fails
 # while still having the build fail if either fails
 
-echo "*******RUNNING BACK-END SMOKE TESTS*******"
+echo "\n\n*******RUNNING BACK-END SMOKE TESTS*******"
 python -m unittest discover -s server-gae/ -p 'Test*.py'
 
 echo "*******RUNNING FRONT-END SMOKE TESTS*******"
