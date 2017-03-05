4799f88fc26ee7d110bcf635d5b0c15c521d79c6
Jeremy Liau <jwl265@mail.usask.ca>
2017-03-05 14:58:08 -0600

    Incorporating filterScreen tests

--- e2e-tests.conf.js
+++ e2e-tests.conf.js
@@ -12,6 +12,7 @@ exports.config = {
     baseUrl: 'http://localhost:8100',
     specs: [
         './tests/e2e-tests/ryans.tests.js'
+        './tests/e2e-tests/filterScreen.tests.js'
     ],
     jasmineNodeOpts: {
         isVerbose: true,
