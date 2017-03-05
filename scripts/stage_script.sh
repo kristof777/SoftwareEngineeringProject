a68313100f3e6354ccc2915ac67d6dd4a2b1548b
Chris Mykota-Reid <clm972@mail.usask.ca>
2017-03-05 16:38:52 -0600

    apparently travis also doesn't know about id3

--- scripts/stage_script.sh
+++ scripts/stage_script.sh
@@ -20,6 +20,9 @@ setup_git(){
   git config --global user.email "clm972@mail.usask.ca"
   git config --global user.name "ChrisMykotaReid"
   git remote set-url origin git@github.com:CMPT371Team1/Project.git
+  git branch id3
+  git checkout id3
+  git pull origin id3
   git branch id3_ReadyForTesting
   git checkout id3_ReadyForTesting
   git pull origin id3_ReadyForTesting
