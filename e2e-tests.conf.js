6a9d6b740c0e2ea2720a4e6ebd00c07cc9d1640c
Jeremy Liau <jwl265@mail.usask.ca>
2017-03-05 13:40:19 -0600

    Specifying ID3 tests
    Need to rename Ryan's test files, will be done later. For now, just need to get the E2E tests working in smoke tests

--- e2e-tests.conf.js
+++ e2e-tests.conf.js
@@ -11,7 +11,7 @@ exports.config = {
     },
     baseUrl: 'http://localhost:8100',
     specs: [
-        './tests/e2e-tests/*.tests.js'
+        './tests/e2e-tests/ryans.tests.js'
     ],
     jasmineNodeOpts: {
         isVerbose: true,
