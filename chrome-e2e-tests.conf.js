4c160d52d94319e148d63728c230446a214d4870
Chris Mykota-Reid <clm972@mail.usask.ca>
2017-03-08 01:28:48 -0600

    created e2e config file for chrome [ci skip]

---
+++ chrome-e2e-tests.conf.js
@@ -0,0 +1,18 @@
+exports.config = {
+    capabilities: {
+        
+        'browserName': 'chrome',
+        'chromeOptions': {
+            args: ['--disable-web-security']
+        },
+        shardTestFiles: true,
+    },
+    baseUrl: 'http://localhost:8100',
+    specs: [
+        './tests/e2e-tests/ryans.tests.js',
+        './tests/e2e-tests/filterScreen.tests.js'
+    ],
+    jasmineNodeOpts: {
+        isVerbose: true,
+    }
+};
