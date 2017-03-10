c85893189caf8d0741f64eb2e810ba88f62b8290
Chris Mykota-Reid <clm972@mail.usask.ca>
2017-03-09 19:29:57 -0600

    forgot path to enc pass

--- scripts/update_server_script.sh
+++ scripts/update_server_script.sh
@@ -4,7 +4,7 @@
 # Logs into our server after a successful build using an encrypted key, and updates
 # the servers repo.
 
-openssl aes-256-cbc -K $encrypted_cb585f974371_key -iv $encrypted_cb585f974371_iv -in server_pass.enc -out server_pass -d
+openssl aes-256-cbc -K $encrypted_cb585f974371_key -iv $encrypted_cb585f974371_iv -in keys/server_pass.enc -out server_pass -d
 export SERVER_PASS='cat server_pass'
 
 sudo apt-get install sshpass
