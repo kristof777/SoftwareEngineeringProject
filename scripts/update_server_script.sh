0722babd50a0c46de01230774a94e2d261ab87d7
Chris Mykota-Reid <clm972@mail.usask.ca>
2017-03-09 18:40:34 -0600

    trying using the password instead

--- scripts/update_server_script.sh
+++ scripts/update_server_script.sh
@@ -4,12 +4,10 @@
 # Logs into our server after a successful build using an encrypted key, and updates
 # the servers repo.
 
-openssl aes-256-cbc -K $encrypted_cb585f974371_key -iv $encrypted_cb585f974371_iv -in keys/server_key.enc -out server_key -d
-chmod 600 server_key
-eval "$(ssh-agent -s)"
-ssh-add server_key 
+openssl aes-256-cbc -K $encrypted_cb585f974371_key -iv $encrypted_cb585f974371_iv -in server_pass.enc -out server_pass -d
+export SERVER_PASS='cat server_pass'
 
-ssh cmpt371g1.usask.ca
+sshpass -p '$SERVER_PASS' ssh gaa721@cmpt371g1.usask.ca
 cd Project
 git pull
 exit
