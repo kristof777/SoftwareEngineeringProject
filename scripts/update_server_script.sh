9f9c9bcd9875657b2b511b7159db3b25c25df402
Chris Mykota-Reid <clm972@mail.usask.ca>
2017-03-09 18:45:18 -0600

    for now we'll install it here

--- scripts/update_server_script.sh
+++ scripts/update_server_script.sh
@@ -7,6 +7,8 @@
 openssl aes-256-cbc -K $encrypted_cb585f974371_key -iv $encrypted_cb585f974371_iv -in server_pass.enc -out server_pass -d
 export SERVER_PASS='cat server_pass'
 
+sudo apt-get install sshpass
+
 sshpass -p '$SERVER_PASS' ssh gaa721@cmpt371g1.usask.ca
 cd Project
 git pull
