2989a91653bfd962361407286d953c1a35a96f1a
Chris Mykota-Reid <clm972@mail.usask.ca>
2017-03-09 20:19:58 -0600

    refactoring scripts for use on build [ci skip]

--- scripts/update_server_script.sh
+++ scripts/update_server_script.sh
@@ -9,6 +9,4 @@ SCRIPT="cd Project; git pull ; exit"
 openssl aes-256-cbc -K $encrypted_cb585f974371_key -iv $encrypted_cb585f974371_iv -in keys/server_pass.enc -out server_pass -d
 export SERVER_PASS=`cat server_pass`
 
-sudo apt-get install sshpass
-
 sshpass -p "${SERVER_PASS}" ssh gaa721@cmpt371g1.usask.ca "${SCRIPT}"
