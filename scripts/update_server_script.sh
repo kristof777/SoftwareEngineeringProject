1659217c6a842de55f414b35f2fc8ec2709e7fbc
Chris Mykota-Reid <clm972@mail.usask.ca>
2017-03-09 19:20:16 -0600

    maybe pass isn't being set

--- scripts/update_server_script.sh
+++ scripts/update_server_script.sh
@@ -6,7 +6,8 @@
 
 openssl aes-256-cbc -K $encrypted_cb585f974371_key -iv $encrypted_cb585f974371_iv -in server_pass.enc -out server_pass -d
 export SERVER_PASS='cat server_pass'
-$SERVER_PASS
+echo "asd"
+echo "$SERVER_PASS"
 sudo apt-get install sshpass
 
 sshpass -p "$SERVER_PASS" ssh gaa721@cmpt371g1.usask.ca
