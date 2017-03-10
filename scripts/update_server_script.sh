1aa9152c4a3566988dd7eabbc15302dbb96dab08
Chris Mykota-Reid <clm972@mail.usask.ca>
2017-03-09 20:09:51 -0600

    this gives a warning but it still looks to be pulling so I think it's fine

--- scripts/update_server_script.sh
+++ scripts/update_server_script.sh
@@ -4,7 +4,7 @@
 # Logs into our server after a successful build using an encrypted key, and updates
 # the servers repo.
 
-SCRIPT="cd Project; git checkout id4; git pull origin/id4; exit"
+SCRIPT="cd Project; git pull ; exit"
 
 openssl aes-256-cbc -K $encrypted_cb585f974371_key -iv $encrypted_cb585f974371_iv -in keys/server_pass.enc -out server_pass -d
 export SERVER_PASS=`cat server_pass`
