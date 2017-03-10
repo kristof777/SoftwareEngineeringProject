c3c8315c403379b6c74141e0218d4c241c91934f
Chris Mykota-Reid <clm972@mail.usask.ca>
2017-03-09 22:22:42 -0600

    conditional run contents if != deployment build [ci skip]

--- scripts/update_server_script.sh
+++ scripts/update_server_script.sh
@@ -2,11 +2,13 @@
 
 # Author: Chris Mykota-Reid
 # Logs into our server after a successful build using an encrypted key, and updates
-# the servers repo.
+# the server's repo.
 
-SCRIPT="cd Project; git pull ; exit"
+if [[ "${BUILD_TYPE}" != "deployment" ]]; then 
+  SCRIPT="cd Project; git pull ; exit"
 
-openssl aes-256-cbc -K $encrypted_cb585f974371_key -iv $encrypted_cb585f974371_iv -in keys/server_pass.enc -out server_pass -d
-export SERVER_PASS=`cat server_pass`
+  openssl aes-256-cbc -K $encrypted_cb585f974371_key -iv $encrypted_cb585f974371_iv -in keys/server_pass.enc -out server_pass -d
+  export SERVER_PASS=`cat server_pass`
 
-sshpass -p "${SERVER_PASS}" ssh gaa721@cmpt371g1.usask.ca "${SCRIPT}"
+  sshpass -p "${SERVER_PASS}" ssh gaa721@cmpt371g1.usask.ca "${SCRIPT}"
+fi
