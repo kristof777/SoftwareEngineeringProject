ce5d3423a64c1c50f56db8f041516ca98e4655e9
Chris Mykota-Reid <clm972@mail.usask.ca>
2017-03-11 13:35:19 -0600

    printing out sys.path for python

--- scripts/smoke_test.sh
+++ scripts/smoke_test.sh
@@ -5,6 +5,7 @@
 # while still having the build fail if either fails
 
 echo $'\n\n\n\n*******RUNNING BACK-END SMOKE TESTS*******'
+python test.py
 python -m unittest discover -s server-gae/ -p 'Test*.py'
 export BACK_END_TEST=$?
 
