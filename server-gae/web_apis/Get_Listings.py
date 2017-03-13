c310e31b9d48cf7f3c44b15e6671ab308a09bd06
Melody Zhao <tiz466@mail.usask.ca>
2017-03-13 15:13:13 -0600

    server testing

--- server-gae/web_apis/Get_Listings.py
+++ server-gae/web_apis/Get_Listings.py
@@ -269,7 +269,7 @@ def get_listingIds_with_numeric_bounds():
                                            Listing.bedrooms <= Listing.numeric_filter_bounds['bedrooms_max'])
     bedrooms_keys = bedroom_query.fetch(keys_only=True)
     bedrooms_keys_len = len(bedrooms_keys)
-    logging.info("bedrooms_keys_len is " + bedrooms_keys_len)
+    logging.info("bedrooms_keys_len is " + str(bedrooms_keys_len))
 
     # query all the listings in db that satisfies the sqft bound condition,
     # only fetch their key(listingId) for efficiency
@@ -277,7 +277,7 @@ def get_listingIds_with_numeric_bounds():
                                         Listing.squarefeet <= Listing.numeric_filter_bounds['sqft_max'])
     sqft_keys = sqft_query.fetch(keys_only=True)
     sqft_keys_len = len(sqft_keys)
-    logging.info("sqft_keys_len is " + sqft_keys_len)
+    logging.info("sqft_keys_len is " + str(sqft_keys_len))
 
     # query all the listings in db that satisfies the price bound condition
     # only fetch their key(listingId) for efficiency
@@ -295,8 +295,8 @@ def get_listingIds_with_numeric_bounds():
 
     # Get the common set of the first two key(listingId) sets
     valid_bd_sqft_keys = list(set(bedrooms_keys) & set(sqft_keys))
-    logging.info("valid_bd_sqft_keys_len is " + len(valid_bd_sqft_keys))
-    assert len(valid_bd_sqft_keys) == min(bedrooms_keys_len, sqft_keys_len)
+    logging.info("valid_bd_sqft_keys_len is " + str(len(valid_bd_sqft_keys)))
+    # assert len(valid_bd_sqft_keys) == min(bedrooms_keys_len, sqft_keys_len)
     bd_sqft_keys_len = len(valid_bd_sqft_keys)
 
     # Get the common set of the next two key(listingId) sets
