d236e72d5d934d356d360b2c91e817a0ace293fd
Chris Mykota-Reid <clm972@mail.usask.ca>
2017-03-16 16:45:10 -0500

    added TODO for new server updation to update_server [ci skip]

--- scripts/update_server.sh
+++ scripts/update_server.sh
@@ -5,9 +5,9 @@
 # the server's repo.
 
 set -ve
-
+# TODO: after git pull do 'gulp test' in TestProject, change git pull to force merge conflicts 
 if [[ "${BUILD_TYPE}" != "deployment" ]]; then 
-  SCRIPT="cd Project; git pull ; exit"
+  SCRIPT="cd Project; git pull; cd ..; cd TestProject; git pull; exit"
 
   openssl aes-256-cbc -K $encrypted_cb585f974371_key -iv $encrypted_cb585f974371_iv -in keys/server_pass.enc -out server_pass -d
   export SERVER_PASS=`cat server_pass`
