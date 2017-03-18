5dbbcf7c3dce1387a444be5fe081b89ca211fdc9
tup916 <tushita.patel@usask.ca>
2017-03-18 17:54:29 -0600

    Added asserts in api_required_fields. This file is code policed now.

--- server-gae/extras/api_required_fields.py
+++ server-gae/extras/api_required_fields.py
@@ -3,8 +3,11 @@ from API_NAME import *
 import utils
 from models.User import User
 
-required_api_dict = {
+"""
+    Creates a dictionary with all fields that are required per API call
+"""
 
+required_api_dict = {
     sign_in_api: ['email', 'password'],
     create_user_api: ['email', 'firstName', 'password', 'confirmedPassword',
                       'phone1', 'province', 'city'],
@@ -29,21 +32,34 @@ required_api_dict = {
 
 
 def check_required_valid(api, post, response, auth_required=False):
+    # TODO Complete this spec, Gaurav
+    """
+    TODO
+    :param api:
+    :param post:
+    :param response:
+    :param auth_required:
+    :return:
+    """
     # validating if request has all required key
     errors, values = utils.keys_missing(required_api_dict[api], post)
 
     # If there exists error then return the response, and stop the function
-    if len(errors) != 0:
+    if len(errors) > 0:
         utils.write_error_to_response(response, errors,
                                       missing_invalid_parameter)
         return False, None
 
     invalid = utils.key_validation(values)
 
-    if len(invalid) != 0:
+    # If there exists an invalidation, return null and stop the function
+    if len(invalid) > 0:
         utils.write_error_to_response(response, invalid,
                                       missing_invalid_parameter)
         return False, None
+
+    assert len(errors) == 0
+    assert len(invalid) == 0
 
     if auth_required:
         user = User.get_by_id(int(values['userId']))
@@ -58,9 +74,13 @@ def check_required_valid(api, post, response, auth_required=False):
         valid_user = user.validate_token(int(values["userId"]),
                                          "auth", values["authToken"])
         if not valid_user:
-            utils.write_error_to_response(response, {not_authorized['error']:
-                                                         "not authorized to create listings"},
+            utils.write_error_to_response(response, {not_authorized['error']: "not authorized to create listings"},
                                           not_authorized['status'])
             return False, None
+
+    assert len(errors) == 0
+    assert len(invalid) == 0
+    assert valid_user
+    assert user is not None
 
     return True, values
