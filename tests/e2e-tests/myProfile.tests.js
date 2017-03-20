a1346ef692d66c89f2ea7cf94d076a7aea942e68
ryantetland <rmt378@mail.usask.ca>
2017-03-19 20:37:21 -0600

    more e2e testing
    Added e2e test for myProfile page.
    Added comments other test

--- tests/e2e-tests/myProfile.tests.js
+++ tests/e2e-tests/myProfile.tests.js
@@ -8,7 +8,6 @@
       jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
   });
 
-  describe('Adding and saving a listing as a user would', function() {
       let originalTimeout;
       browser.get('/#/ionic-lab');
 
@@ -26,82 +25,45 @@
 
         attemptSignIn('test1@test.com', '123abcABC');
         sleep();
-
-        expect(element(by.id('goToFilters'))).isDisplayed;
-   });
-
-    it ('Should go to add listing page', function(){
-        let myListings = element(by.id('tab-t0-3'));
-        myListings.click();
-        sleep();
+    });
+    it ('Should go to myProfile page', function(done){
+        let myProfileTab = element(by.id('tab-t0-2'));
+        myProfileTab.click();
         sleep();
 
+        let email = element(by.id('settingsEmail')).all(by.tagName('input')).first();
+        let firstName = element(by.id('settingsFirstName')).all(by.tagName('input')).first();
+        let lastName = element(by.id('settingsLastName')).all(by.tagName('input')).first();
+        let phoneNumber = element(by.id('settingsPhoneNumber')).all(by.tagName('input')).first();
+        let secondPhoneNumber = element(by.id('settingsSecondPhoneNumber')).all(by.tagName('input')).first();
+        let provinces = element(by.id('settingsProvince'));
 
-        let addListingButton= element(by.id('addButton'));
-        addListingButton.click();
-        sleep();
+
+        let city = element(by.id('settingsCity')).all(by.tagName('input')).first();
+
+        email.sendKeys('test1@test.com');
+        firstName.sendKeys('Tester #1');
+        lastName.sendKeys('lastName');
+        phoneNumber.sendKeys('3633126691');
+        secondPhoneNumber.sendKeys('3065551234');
+        provinces.click();
         sleep();
 
-        expect(element(by.buttonText('Add Image')).getText()).toEqual('ADD IMAGE');
+        selectProvince('Saskatchewan');
+
+        city.sendKeys('Saskatoon');
+        let saveButton = element(by.id('settingsSave'));
+        saveButton.click();
+        sleep();
+
+        //let okButton2 = element(by.id('Ok'));
+        //okButton2.click();
+        //sleep();
+
+        done();
     });
 
-    it ('Should add a listing', function(done){
-        let provinceDropList = element(by.id('alProvince'));
-        provinceDropList.click();
 
-        sleep();
-
-        /*let skOption = element(by.buttonText('Saskatchewan'));
-        browser.executeScript("arguments[0].scrollIntoView();", skOption);
-        skOption.click();
-        sleep();
-        let okaybtn = element(by.buttonText('OK'));
-        okaybtn.click();
-      */  sleep();
-
-      selectProvince('Saskatchewan');
-
-        let bath = element(by.id('alBathroom')).all(by.tagName('input')).first();
-
-
-        let city = element(by.id('alCityTown')).all(by.tagName('input')).first();
-        city.sendKeys('Saskatoon');
-
-        let address = element(by.id('alAddress')).all(by.tagName('input')).first();
-        address.sendKeys('123 First Street East');
-
-        let postalCode = element(by.id('alPostalCode')).all(by.tagName('input')).first();
-        postalCode.sendKeys('f1s 9u8');
-
-        browser.executeScript("arguments[0].scrollIntoView();", bath);
-
-        let price = element(by.id('alPrice')).all(by.tagName('input')).first();
-        price.sendKeys('13000000');
-
-        let feet = element(by.id('alSqft')).all(by.tagName('input')).first();
-        feet.sendKeys('600');
-
-        let bed = element(by.id('alBedroom')).all(by.tagName('input')).first();
-        bed.sendKeys('5');
-
-        bath.sendKeys('2');
-
-        sleep();
-
-        let desc2 = element(by.id('alDesc')).all(by.tagName('textarea')).first();
-        sleep();
-        desc2.sendKeys('Nice trailer with a little dust. Fixer Upper.');
-
-        let saveButton = element(by.id('alSave'));
-        saveButton.click();
-
-        sleep();
-        done();
-
-        });
-
-
-});
 
 
 /**
