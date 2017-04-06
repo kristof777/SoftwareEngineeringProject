370f19fa094f1dda28ca6fef0173f4b0bcdcae36
justin <ham896@mail.usask.ca>
2017-04-06 12:43:03 -0600

    Updated database to Actual database.

--- src/app/kasper-config.ts
+++ src/app/kasper-config.ts
@@ -1,6 +1,6 @@
 export class KasperConfig {
     static DB_INFO: any = {name: "kasper.db", location: 'default'};
-    static API_URL: string = "http://cmpt371g1.usask.ca:4040";
+    static API_URL: string = "http://cmpt371g1.usask.ca:8080";
 
 
 
